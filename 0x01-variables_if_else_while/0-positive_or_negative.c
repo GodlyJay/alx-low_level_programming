9f0039774959f2adbef34c45a7014d5eca7597dc
Your Name <you@example.com>
2023-03-23 03:37:38 -0700

    Error fle

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+/*
+ * main - this fnx is main
+ * Return: returns 0
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("The number is positive");
+	}
+	else if (n == 0)
+	{
+		printf("The number is 0");
+	}
+	else 
+	{
+		printf("The number is negative");
+	}
+	return (0);
+}
